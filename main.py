4923ee749376f84e311c7df47f7b19e6bdc03dc5
txxgxxx <taegun1020@naver.com>
2024-01-06 19:05:16 +0900

    6.4 Inheritance

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
+class Dog:
 
-class GuardDog:
-
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, age):
         self.name = name
-        self.age = 5
         self.breed = breed
+        self.age = age
+
+
+class GuardDog(Dog):
 
     def grr(self):
         print("stay away!")
 
-class Puppy:
-
-    def __init__(self, name, breed):
-        self.name = name
-        self.age = 0.1
-        self.breed = breed
-
-    def __str__(self):
-        return f"{self.breed} named {self.name}"
+class Puppy(Dog):
     
     def woof_woof(self):
         print("Woof Woof!")
 
-    def introduce(self):
-        self.woof_woof()
-        print(f"my name is {self.name} and i am a baby {self.breed}")
-        self.woof_woof()
+   
 
 ruffus = Puppy(name="Ruffus", breed="Beagle")
 bibi = Puppy(name="Bibi", breed="Dalmatian")
