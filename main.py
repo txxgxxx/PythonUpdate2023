bf96eca69144bf5dae11cc8f195b5d18a908df70
txxgxxx <taegun1020@naver.com>
2024-01-10 07:06:35 +0900

    7.5 pagination

--- main.py
+++ main.py
@@ -3,26 +3,37 @@ from bs4 import BeautifulSoup
 
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-
-jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
-
 all_jobs = []
 
-for job in jobs:
-    title = job.find("span", class_="title").text
-    company, position, region = job.find_all("span", class_="company")
-    url = job.find("div", class_="tooltip").next_sibling["href"]
-    job_data = {
-        "title": title,
-        "company": company.text,
-        "position": position.text,
-        "region": region.text,
-        "url": f"https://weworkremotely.com{url}",
-    }
-    all_jobs.append(job_data)
+def scrape_page(url):
+    print(f"Scrapping {url}...")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+    for job in jobs:
+        title = job.find("span", class_="title").text
+        company, position, region = job.find_all("span", class_="company")
+        url = job.find("div", class_="tooltip").next_sibling["href"]
+        job_data = {
+            "title": title,
+            "company": company.text,
+            "position": position.text,
+            "region": region.text,
+            "url": f"https://weworkremotely.com{url}",
+        }
+        all_jobs.append(job_data)
 
-print(all_jobs)
+def get_pages(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for page_number in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={page_number+1}"
+    scrape_page(url)
+
+print(len(all_jobs))
