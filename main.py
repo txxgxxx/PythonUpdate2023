971c125f3860496b5015f2c7d0c0dfa798fcc102
txxgxxx <taegun1020@naver.com>
2024-01-10 08:01:49 +0900

    7.6 Code Challenge

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
-
 all_jobs = []
 
-def scrape_page(url):
-    print(f"Scrapping {url}...")
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
-    for job in jobs:
-        title = job.find("span", class_="title").text
-        company, position, region = job.find_all("span", class_="company")
-        url = job.find("div", class_="tooltip").next_sibling["href"]
-        job_data = {
-            "title": title,
-            "company": company.text,
-            "position": position.text,
-            "region": region.text,
-            "url": f"https://weworkremotely.com{url}",
-        }
-        all_jobs.append(job_data)
+keywords = ["flutter", "python", "golang"]
 
-def get_pages(url):
-    response = requests.get(url)
+class Scraper:
+    
+    
+    def scrap_jobs(self):
+       soup = BeautifulSoup(self.response, "html.parser")
+       jobs = soup.find("table", id="jobsboard").find_all("tr", class_="job")
+       for job in jobs:
+           job_info = job.find("td", class_="company")
+           a = job_info.find("a")
+           url = a["href"]
+           title = a.find("h2", attrs={"itemprop":"title"})
+           company = job_info.find("span", class_="companyLink").find("h3", attrs={"itemprop":"name"})
+           location = job_info.find("span", class_="companyLink").next_sibling.next_sibling
+           job_data = {
+               "title": title.text.strip(),
+               "company": company.text.strip(),
+               "location": location.text.strip(),
+               "link": f"https://remoteok.com{url}",
+               "keyword": self.keyword
+           }
+           all_jobs.append(job_data)
+        
+    def show_jobs(self):
+        filtered_jobs = [d for d in all_jobs if self.type in d.get("keyword", "")]
+        print(filtered_jobs)
+    
+    def __init__(self, keyword):
+        self.type= keyword
+        response = requests.get(f"https://remoteok.com/remote-{keyword}-jobs", headers={
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        })
+        self.response = response.content
+        self.keyword = keyword
 
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
 
-total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
 
-for page_number in range(total_pages):
-    url = f"https://weworkremotely.com/remote-full-time-jobs?page={page_number+1}"
-    scrape_page(url)
+python = Scraper(keywords[1])
 
-print(len(all_jobs))
+python.scrap_jobs()
+
+python.show_jobs()
+
+golang = Scraper(keywords[2])
+
+golang.scrap_jobs()
+
+golang.show_jobs()
+
+print(all_jobs)
