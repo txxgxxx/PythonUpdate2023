c983ea1c32f895323f342ad18585422cfd5af4df
txxgxxx <taegun1020@naver.com>
2024-01-10 06:49:16 +0900

    7.0 - 7.4

--- main.py
+++ main.py
@@ -1,56 +1,28 @@
-class Player:
+import requests
+from bs4 import BeautifulSoup
 
-    def __init__(self, name, team):
-        self.name = name
-        self.xp = 1500
-        self.team = team
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-    def introduce(self):
-        print(f"Hello! I'm {self.name} and I play for {self.team}")
+response = requests.get(url)
 
-class Team:
+soup = BeautifulSoup(response.content, "html.parser")
 
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.players = []
 
-    def add_player(self, name):
-        new_player = Player(name, self.team_name)
-        self.players.append(new_player)
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
-    def remove_player(self, name):
-        for remove_player in self.players:
-            if remove_player.name == name:
-                self.players.remove(remove_player)
-                print(f"{name} was expelled from the team {self.team_name}")
-                return
-        print("Cant find player")
-    
-    def team_xp(self):
-        team_xp = 0
-        for player in self.players:
-            team_xp += player.xp
-            print(f"🍀 {player.name} contributes || {player.xp} || xp for {self.team_name}! \n Thank you for your dedication.")
-        print(f"🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉 \n {self.team_name}'s total xp is {team_xp} \n🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
-        
-    
-    def show_players(self):
-        for player in self.players:
-            player.introduce()
+all_jobs = []
 
-team_t1 = Team("SKT T1")
+for job in jobs:
+    title = job.find("span", class_="title").text
+    company, position, region = job.find_all("span", class_="company")
+    url = job.find("div", class_="tooltip").next_sibling["href"]
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "position": position.text,
+        "region": region.text,
+        "url": f"https://weworkremotely.com{url}",
+    }
+    all_jobs.append(job_data)
 
-team_t1.add_player("geon")
-team_t1.add_player("Faker")
-
-team_t1.remove_player("geon")
-team_t1.show_players()
-team_t1.add_player("geon")
-team_t1.add_player("Oner")
-
-team_blue = Team("DRX")
-
-team_blue.add_player("Beryl")
-
-team_t1.team_xp()
-team_blue.team_xp()
+print(all_jobs)
